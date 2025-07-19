ab02e89817a20f6fe268c9db9beb300739e64e33
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-07-19 10:01:31 -0400

    update beamscaler, should work now

--- hdl_v2/beamscaler_wrap.sv
+++ hdl_v2/beamscaler_wrap.sv
@@ -1,4 +1,21 @@
 `timescale 1ns / 1ps
+`define DLYFF #0.1
+// 12-bit scaler module. This effectively
+// uses 0.5 DSPs/scaler, which is double
+// what would be needed because we swap
+// between DSPs during an update period.
+// The DSP usage isn't enough that we care,
+// and using the cascade output path allows
+// us to avoid a ton of registers and wiring.
+//
+// If you free-read from this, there's no
+// guarantee you'll always be reading the
+// same set of data unless you somehow
+// sync yourself to the update period.
+// 
+// According to the XPM docs scal_dat_o should 
+// be captured 2 clocks after scal_rd_i.
+// Who knows, though.
 module beamscaler_wrap #(parameter NBEAMS = 2,
                          localparam NSCALERS = 2,
                          parameter TIME_COUNT = 100000000,
@@ -10,9 +27,11 @@ module beamscaler_wrap #(parameter NBEAMS = 2,
                          output done_o,
                          
                          input wb_clk_i,
+                         input wb_rst_i,
                          // 96 scalers needs 7 bit addr
+                         input scal_rd_i,
                          input [6:0] scal_adr_i,
-                         output [11:0] scal_dat_o);
+                         output [31:0] scal_dat_o);
 
     localparam NDUALBEAMS = (NBEAMS/2) + (NBEAMS%2);    
     
@@ -51,7 +70,10 @@ module beamscaler_wrap #(parameter NBEAMS = 2,
     // On the read side, the active read bank is the low address.
     reg [7:0] scaler_addr = {8{1'b0}};
     reg active_write_bank = 0;
-        
+
+    reg [1:0] scaler_write = {2{1'b0}};
+
+    reg scaler_write_any = 0;        
     // this is combinatoric, it's easier to let Vivado
     // recode the FSM state as needed
     reg [2:0] beamscaler_state;
@@ -83,72 +105,89 @@ module beamscaler_wrap #(parameter NBEAMS = 2,
     reg [47:0] timer = {48{1'b0}};
         
     reg timer_complete = 0;
+    reg update_done = 0;
     
     always @(posedge wb_clk_i) begin
+        // this actually does an extra write into the top address, but that's
+        // always unused so I *do not care*.
+        if (state == RESET || state == IDLE_B) scaler_write[0] <= 0;
+        else if (state == DATA_SHIFT_A) scaler_write[0] <= 1;
+    
+        if (state == RESET || state == IDLE_A) scaler_write[1] <= 0;
+        else if (state == DATA_SHIFT_B) scaler_write[1] <= 1;
+    
+        if (state == RESET || state == IDLE_A || state == IDLE_B) scaler_write_any <= 0;
+        else if (state == DATA_SHIFT_A || state == DATA_SHIFT_B) scaler_write_any <= 1;
+    
         if (timer_complete|| state == RESET || state == RESET_PREP_A)
-            timer <= {48{1'b0}};
+            timer <= `DLYFF {48{1'b0}};
         else
-            timer <= timer + 1;
+            timer <= `DLYFF timer + 1;
         
-        timer_complete <= (timer == TIME_COUNT);
+        timer_complete <= `DLYFF (timer == TIME_COUNT);
+
+        update_done <= `DLYFF (state == DATA_SHIFT_A || state == DATA_SHIFT_B) && scaler_addr[7];
         
         // just transition at the idle points. The read banks are the opposite
         // of this.
-        if (state == IDLE_A) active_write_bank <= 0;
-        else if (state == IDLE_B) active_write_bank <= 1;
+        if (state == IDLE_A) active_write_bank <= `DLYFF 0;
+        else if (state == IDLE_B) active_write_bank <= `DLYFF 1;
         
         if (state == IDLE_A || state == IDLE_B)
-            scaler_addr <= NDUALBEAMS-1;
+            scaler_addr <= `DLYFF NDUALBEAMS-1;
         else if (state == DSP_SHIFT_A || state == DSP_SHIFT_B)
-            scaler_addr <= scaler_addr[6:0] - 1;
+            scaler_addr <= `DLYFF scaler_addr[6:0] - 1;
             
-        case (state)
-            RESET:  state <= RESET_PREP_A;
-            RESET_PREP_A: state <= IDLE_A;
-            IDLE_A: if (timer_complete) state <= PREP_B;
-            PREP_B: state <= COMPUTE_A_0;
-            COMPUTE_A_0: state <= COMPUTE_A_1;
-            COMPUTE_A_1: state <= DATA_SHIFT_A;
-            DATA_SHIFT_A: if (scaler_addr[7]) state <= IDLE_B;
-                          else state <= DSP_SHIFT_A;
-            DSP_SHIFT_A: state <= DATA_SHIFT_A;
-            IDLE_B: if (timer_complete) state <= PREP_A;
-            PREP_A: state <= COMPUTE_B_0;
-            COMPUTE_B_0: state <= COMPUTE_B_1;
-            COMPUTE_B_1: state <= DATA_SHIFT_B;
-            DATA_SHIFT_B: if (scaler_addr[7]) state <= IDLE_A;
-                          else state <= DSP_SHIFT_B;
-            DSP_SHIFT_B: state <= DATA_SHIFT_B;
-        endcase
-    // the CE logic might be easier as an on/off:
-    // -> 1 in RESET_PREP_A or PREP_A or COMPUTE_A_0 or DATA_SHIFT_A
-    // -> 0 in PREP_B or COMPUTE_A_1 or DSP_SHIFT_A or IDLE_B (to catch the exit) or RESET
+        if (wb_rst_i) state <= `DLYFF RESET;
+        else begin
+            case (state)
+                RESET:  state <= `DLYFF RESET_PREP_A;
+                RESET_PREP_A: state <= `DLYFF IDLE_A;
+                IDLE_A: if (timer_complete) state <= `DLYFF PREP_B;
+                PREP_B: state <= `DLYFF COMPUTE_A_0;
+                COMPUTE_A_0: state <= `DLYFF COMPUTE_A_1;
+                COMPUTE_A_1: state <= `DLYFF DATA_SHIFT_A;
+                DATA_SHIFT_A: if (scaler_addr[7]) state <= `DLYFF IDLE_B;
+                              else state <= `DLYFF DSP_SHIFT_A;
+                DSP_SHIFT_A: state <= `DLYFF DATA_SHIFT_A;
+                IDLE_B: if (timer_complete) state <= `DLYFF PREP_A;
+                PREP_A: state <= `DLYFF COMPUTE_B_0;
+                COMPUTE_B_0: state <= `DLYFF COMPUTE_B_1;
+                COMPUTE_B_1: state <= `DLYFF DATA_SHIFT_B;
+                DATA_SHIFT_B: if (scaler_addr[7]) state <= `DLYFF IDLE_A;
+                              else state <= `DLYFF DSP_SHIFT_B;
+                DSP_SHIFT_B: state <= `DLYFF DATA_SHIFT_B;
+            endcase
+        end            
+        // the CE logic might be easier as an on/off:
+        // -> 1 in RESET_PREP_A or PREP_A or COMPUTE_A_0 or DATA_SHIFT_A
+        // -> 0 in PREP_B or COMPUTE_A_1 or DSP_SHIFT_A or IDLE_B (to catch the exit) or RESET
         if (state == PREP_B || state == COMPUTE_A_1 || 
             state == DSP_SHIFT_A || state == IDLE_B || state == RESET)
-            beamscaler_ce[0] <= 0;
+            beamscaler_ce[0] <= `DLYFF 0;
         else if (state == RESET_PREP_A || state == PREP_A || state == COMPUTE_A_0 || state == DATA_SHIFT_A)
-            beamscaler_ce[0] <= 1;
+            beamscaler_ce[0] <= `DLYFF 1;
 
         if (state == PREP_A || state == COMPUTE_B_1 ||
             state == DSP_SHIFT_B || state == IDLE_A || state == RESET)
-            beamscaler_ce[1] <= 0;
+            beamscaler_ce[1] <= `DLYFF 0;
         else if (state == PREP_B || state == COMPUTE_B_0 || state == DATA_SHIFT_B)
-            beamscaler_ce[1] <= 1;            
+            beamscaler_ce[1] <= `DLYFF 1;            
         
-        if (state == DATA_SHIFT_A) dsp_capture_A <= final_out[0][23:0];
-        else if (state == DSP_SHIFT_A) dsp_capture_A <= final_out[0][47:24];
+        if (state == DATA_SHIFT_A) dsp_capture_A <= `DLYFF final_out[0][23:0];
+        else if (state == DSP_SHIFT_A) dsp_capture_A <= `DLYFF final_out[0][47:24];
         
-        if (state == DATA_SHIFT_B) dsp_capture_B <= final_out[1][23:0];
-        else if (state == DSP_SHIFT_B) dsp_capture_B <= final_out[1][47:24];
+        if (state == DATA_SHIFT_B) dsp_capture_B <= `DLYFF final_out[1][23:0];
+        else if (state == DSP_SHIFT_B) dsp_capture_B <= `DLYFF final_out[1][47:24];
     end        
 
     wire ifclk_ce;
-    wire wbclk_ce;
+    wire wb_clk_ce;
     
-    // check this we might need to expand it
-    clk_div_ce #(.CLK_DIVIDE(3)) u_ce_gen(.clk(ifclk_i),
+    // divide by 7 is the max we can do in 3 bits
+    clk_div_ce #(.CLK_DIVIDE(6)) u_ce_gen(.clk(ifclk_i),
                                           .ce(ifclk_ce));
-    flag_sync u_ce_sync(.in_clkA(ifclk_ce),.out_clkB(wbclk_ce),
+    flag_sync u_ce_sync(.in_clkA(ifclk_ce),.out_clkB(wb_clk_ce),
                         .clkA(ifclk_i),.clkB(wb_clk_i));                 
 
     generate
@@ -196,5 +235,35 @@ module beamscaler_wrap #(parameter NBEAMS = 2,
             end
         end
     endgenerate
+
+    wire [63:0] ram_dina = { {4{1'b0}}, dsp_capture_B[12 +: 12],
+                             {4{1'b0}}, dsp_capture_B[0 +: 12],
+                             {4{1'b0}}, dsp_capture_A[12 +: 12],
+                             {4{1'b0}}, dsp_capture_A[0 +: 12] };
+    wire [7:0] ram_wea = {   {4{scaler_write[1]}},
+                             {4{scaler_write[0]}} };
+    wire ram_ena = scaler_write_any;
+    
+    // ALL WE HAVE LEFT IS THE ACTUAL RAM!!!
+    // NO WE CANNOT INFER THIS, IT AIN'T SUPPORTED
+    // 7 bit on write side
+    // 8 bit on read side
+    xpm_memory_sdpram #(.ADDR_WIDTH_A(7),
+                        .ADDR_WIDTH_B(8),
+                        .READ_DATA_WIDTH_B(32),
+                        .WRITE_DATA_WIDTH_A(64),
+                        .BYTE_WRITE_WIDTH_A(8),
+                        .MEMORY_SIZE(8192))
+        u_scaler_ram(.clka(wb_clk_i),
+                     .dina(ram_dina),
+                     .addra(scaler_addr),
+                     .wea(ram_wea),
+                     .ena(ram_ena),
+                     .rstb(wb_rst_i),
+                     .addrb({scal_adr_i, !active_write_bank}),
+                     .doutb(scal_dat_o),
+                     .enb(scal_rd_i),
+                     .regceb(1'b1));
+    
+    assign done_o = update_done;
 endmodule
-                         
