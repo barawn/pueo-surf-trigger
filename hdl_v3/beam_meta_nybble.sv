0cbd6f791bb93ac79e37bf203b70f43743900965
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-09-05 12:03:23 -0400

    start building up v3 tooling

---
+++ hdl_v3/beam_meta_nybble.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/04/2025 11:20:38 AM
+// Design Name: 
+// Module Name: beam_meta_nybble
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module beam_meta_nybble(
+
+    );
+endmodule
